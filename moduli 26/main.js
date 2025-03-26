deb3b99727591be764e1fbee88e3ae4cead443d9
FlorianGashi <floriangashi812@gmail.com>
2025-03-26 12:44:26 -0700

    22

---
+++ moduli 26/main.js
@@ -0,0 +1,21 @@
+console.log("kampion");
+var a =50 + 50;
+console.log(a);
+var b= 20;
+var c = 30;
+var d= b+c;
+console.log (d);
+var e = 20;
+var f = 10;
+var g = e-f;
+console.log(g);
+var h = 50;
+var i = 10;
+var rezultati =h/i;
+console.log(rezultati)
+var n=100
+n++;
+console.log(n);
+var m= 100;
+m--;
+console.log(m);
