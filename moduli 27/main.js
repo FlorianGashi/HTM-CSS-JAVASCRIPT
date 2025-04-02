558d6c9f8dffc81e57fe267f62125b60828afb0d
FlorianGashi <floriangashi812@gmail.com>
2025-04-02 11:47:25 -0700

    mod 27

---
+++ moduli 27/main.js
@@ -0,0 +1,28 @@
+function showMessage(){
+    alert("Hello")
+}
+showMessage();
+function sum(num1,num2){
+    return num1+num2;
+}
+console.log (28+2)
+function dsFuntcion(){
+    var localVar="Shkolla digjitale"
+    alert(localVar);
+}
+dsFuntcion();
+function tosecond(minutes ){
+    return minutes *60;
+}
+console.log(tosecond( 60 ));
+function siperfaqjaetrekendeshit(a,h){
+    return 0.5 *a*h;
+}
+console.log(siperfaqjaetrekendeshit(1,6));
+var vetura = {
+    name:"llad",
+    color:"sahrit",
+    km:"1000000",
+    year:parakrishtit
+}
+alert (vetura.name);
