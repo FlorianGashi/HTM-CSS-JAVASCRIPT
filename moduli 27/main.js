984688437813b9a899575e433929b835daf2505d
FlorianGashi <floriangashi812@gmail.com>
2025-04-14 11:34:01 -0700

    babababbabbababab

--- moduli 27/main.js
+++ moduli 27/main.js
@@ -22,7 +22,39 @@ console.log(siperfaqjaetrekendeshit(1,6));
 var vetura = {
     name:"llad",
     color:"sahrit",
-    km:"1000000",
-    year:parakrishtit
-}
+    year:"parakrishtit",
+    km:100000,
+    get getkm(){
+        return this.km;
+    },
+
+
+
+    set setkm(km){
+        this.km=km;
+    }
+};
+console.log(vetura.getkm)
+vetura.setkm=100;
+console.log(vetura.getkm);
+//     startengine:function(){
+//         alert("start");
+//     }
+// };
+vetura.startengine();
 alert (vetura.name);
+
+var shkolla={
+    name:"shkolla digjitale",
+    viti_ndertimit:"2002",
+    nexenesit:"2000",
+}
+alert (shkolla.name);
+var computer = new Object();
+computer.name = "lenov";
+computer.CPU = "Intel";
+computer.RAM = "16GB";
+computer.type = function(){
+    return this.name + " " + this.CPU + " " + this.RAM;
+}
+alert(computer.CPU);
