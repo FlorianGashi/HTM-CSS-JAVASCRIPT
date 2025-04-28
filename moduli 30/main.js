ab0666f4ea4b57b377d2057926cc3a55de757e33
FlorianGashi <floriangashi812@gmail.com>
2025-04-28 11:48:58 -0700

    30

---
+++ moduli 30/main.js
@@ -0,0 +1,23 @@
+var x=5;
+if (x>2){
+    console.log("x eshte me i madh se 2")
+}
+else {
+    console.log("x eshte me i vogel se 2")
+}
+var input=document.getElementById('input_id')
+var button=document.getElementById('btn_id')
+var text=document.getElementById('text_id')
+
+button.onclick=function(){
+    text.innerHTML=input.value;
+}
+var input1=document.querySelector('#input1_id')
+var input2=document.querySelector('#input2_id')
+var button=document.querySelector('#btn2_id')
+var answer=document.querySelector('#answer')
+button.addEventListener("click",function(){
+    answer.innerHTML= parseInt(input1.value)+parseInt(input2.value);
+}
+
+);
