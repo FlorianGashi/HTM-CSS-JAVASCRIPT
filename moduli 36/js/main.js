0ed99c47b7d3c5cd630c561ae260653321cd9a1c
FlorianGashi <floriangashi812@gmail.com>
2025-05-28 11:11:30 -0700

    VAZHDIM

--- moduli 36/js/main.js
+++ moduli 36/js/main.js
@@ -17,3 +17,15 @@ $('#btn').click(function(){
     $('#h1').text('testing text');
     $('#h1').append('Extra time');
 })
+$("#btn3").click(function(){
+    $(".hidden").show('slow');
+});
+$("#btn4").click(function(){
+    $(".hidden").hide();
+});
+$('#square').click(function(){
+    $('#square').animate({
+        'width':'200px', 
+        'height':'200px'
+    });
+});
